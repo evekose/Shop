a6bc1669dea5dbf08083ce63ce944b100fc8dd07
Eve <evekose@gmail.com>
2022-11-24 11:30:29 +0200

    DbContextSpaceshipMigration

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ShopContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
