b49e7a6059e8ee2a3f838c68ed21b2f8c1422ee9
Eve <evekose@gmail.com>
2023-01-07 13:56:29 +0200

    ShopCore.Domain, CarDto, Shop.Models, ViewModels

---
+++ Shop.Core/Domain/Car/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Domain.Car
+{
+    public class Car
+    {
+        [Key]
+
+        public Guid? Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string NameOfOwner { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+        public int EnginePower { get; set; }
+        public int Mileage { get; set; }
+        public DateTime BuiltDate { get; set; }
+        public DateTime MaintanceDate { get; set; }
+    }
+}
---
+++ Shop/Models/Car/CarDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Shop.Models.Car
+{
+    public class CarDetailsViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string NameOfOwner { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+        public int EnginePower { get; set; }
+        public int Mileage { get; set; }
+        public DateTime BuiltDate { get; set; }
+        public DateTime MaintanceDate { get; set; }
+    }
+}
