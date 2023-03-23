9416690906b3aaeece1bc586b418a15c5b6a45a1
Eve <evekose@gmail.com>
2023-03-23 17:43:49 +0200

    NewTests

--- Shop.SpaceshipTest/SpaceshipTest.cs
+++ Shop.SpaceshipTest/SpaceshipTest.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using Shop.Core.Dto;
 using Shop.Core.ServiceInterface;
 using Xunit;
+using Shop.Core.Domain.Spaceship;
+using System.Xml.Linq;
 
 namespace Shop.SpaceshipTest
 {
@@ -73,29 +75,114 @@ namespace Shop.SpaceshipTest
 		{
 			Guid guid = Guid.Parse("15b18d85-bb40-4dbe-9093-cc910f22df43");
 
-			SpaceshipDto spaceship = new SpaceshipDto();
+			SpaceshipDto spaceship = MockSpaceshipData();
+
+			var addSpaceship = await Svc<ISpaceshipsServices>().Create(spaceship);
+			var result = await Svc<ISpaceshipsServices>().Delete((Guid)addSpaceship.Id);
+
+			Assert.Equal(result, addSpaceship);
+
+		}
+
+		[Fact]
+		private async Task Should_UpdateSpaceship_WhenUpdateData()
+		{
+			var guid = new Guid("15b18d85-bb40-4dbe-9093-cc910f22df43");
+
+			Spaceship spaceship = new Spaceship();
+			SpaceshipDto dto = MockSpaceshipData();
 
 			spaceship.Id = Guid.Parse("15b18d85-bb40-4dbe-9093-cc910f22df43");
-			spaceship.Name = "asd";
+			spaceship.Name = "Name123";
 			spaceship.Type = "asd";
 			spaceship.Crew = 123;
-			spaceship.Passengers = 123;
-			spaceship.CargoWeight = 123;
-			spaceship.FullTripsCount = 123;
-			spaceship.MaintenanceCount = 1000;
-			spaceship.LastMaintenance = DateTime.Now;
-			spaceship.EnginePower = 1000;
-			spaceship.MaidenLaunch = DateTime.Now;
-			spaceship.BuiltDate = DateTime.Now;
-			spaceship.CreatedAt = DateTime.Now;
-			spaceship.ModifiedAt = DateTime.Now;
+			spaceship.Passengers = 123123;
+			spaceship.CargoWeight = 123123;
+			spaceship.FullTripsCount = 123123;
+			spaceship.MaintenanceCount = 100012;
+			spaceship.LastMaintenance = DateTime.Now.AddYears(1);
+			spaceship.EnginePower = 1000123;
+			spaceship.MaidenLaunch = DateTime.Now.AddYears(1);
+			spaceship.BuiltDate = DateTime.Now.AddYears(1);
+			spaceship.CreatedAt = DateTime.Now.AddYears(1);
+			spaceship.ModifiedAt = DateTime.Now.AddYears(1);
 
-			await Svc<ISpaceshipsServices>().Delete(guid);
+			//var spaceshipToUpdate = new SpaceshipDto()
+			//{
+			//	Name = "Name123",
+			//	Type = "asd",
+			
+			//};
+		
 
-			Assert.Equal(guid, spaceship.Id);
+			await Svc<ISpaceshipsServices>().Update(dto);
+
+			Assert.Equal(spaceship.Id, guid);
+			Assert.DoesNotMatch(spaceship.Name, dto.Name);
+			Assert.DoesNotMatch(spaceship.EnginePower.ToString(), dto.EnginePower.ToString());
+			Assert.Equal(spaceship.Crew, dto.Crew);
+		}
 
 
+		private SpaceshipDto MockSpaceshipData()
+		{
+			SpaceshipDto spaceship = new()
+			{
+				Name = "Name",
+				Type = "asd",
+				Crew = 123,
+				Passengers = 123,
+				CargoWeight = 123,
+				FullTripsCount = 123,
+				MaintenanceCount = 1000,
+				LastMaintenance = DateTime.Now,
+				EnginePower = 1000,
+				MaidenLaunch = DateTime.Now,
+				BuiltDate = DateTime.Now,
+				CreatedAt = DateTime.Now,
+				ModifiedAt = DateTime.Now,
+			};
 
+			return spaceship;
+		}
+
+		[Fact]
+		private async Task Should_UpdateSpaceship_WhenUpdateDataVersion2()
+		{
+			SpaceshipDto dto = MockSpaceshipData();
+			var createSpaceship = await Svc<ISpaceshipsServices>().Create(dto);
+
+			SpaceshipDto update = MockUpdateSpaceship();
+			var result = await Svc<ISpaceshipsServices>().Update(update);
+
+			Assert.Equal(update.Id, dto.Id);
+			Assert.DoesNotMatch(result.Name, createSpaceship.Name);
+			Assert.DoesNotMatch(result.EnginePower.ToString(), createSpaceship.EnginePower.ToString());
+			Assert.Equal(result.Crew, createSpaceship.Crew);
+			Assert.NotEqual(result.ModifiedAt, createSpaceship.ModifiedAt);
+		}
+
+
+		private SpaceshipDto MockUpdateSpaceship()
+		{
+			SpaceshipDto update = new()
+			{
+				Name = "Name123",
+				Type = "asd",
+				Crew = 123,
+				Passengers = 123123,
+				CargoWeight = 123123,
+				FullTripsCount = 123123,
+				MaintenanceCount = 100012,
+				LastMaintenance = DateTime.Now.AddYears(1),
+				EnginePower = 1000123,
+				MaidenLaunch = DateTime.Now.AddYears(1),
+				BuiltDate = DateTime.Now.AddYears(1),
+				CreatedAt = DateTime.Now.AddYears(1),
+				ModifiedAt = DateTime.Now.AddYears(1),
+			};
+
+			return update;
 		}
 	}
 }
